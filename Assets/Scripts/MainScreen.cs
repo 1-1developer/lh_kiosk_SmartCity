3ee717c6a08b1a940c0c2fa4860161f3aae386fe
HooJungKim <sdhh1735@gmail.com>
2023-08-28 16:05:56 +0900

    실행파일

--- Assets/Scripts/MainScreen.cs
+++ Assets/Scripts/MainScreen.cs
@@ -5,8 +5,7 @@ using UnityEngine.UIElements;
 
 public class MainScreen : MenuScreen
 {
-    const string MS_POPUP1 = "MS_Popup1";
-    const string MS_POPUP2 = "MS_Popup2";
+    const string MS_POPUP = "MS_Popup";
     const string MS_STATE0 = "MS_State0";
     const string MS_STATE1 = "MS_State1";
     const string MS_BUTTON1 = "MS_Button1";
@@ -15,8 +14,7 @@ public class MainScreen : MenuScreen
     [SerializeField]
     TimeManager Timer;
 
-    VisualElement m_MS_Popup1;
-    VisualElement m_MS_Popup2;
+    VisualElement m_MS_Popup;
     VisualElement m_MS_State0;
     VisualElement m_MS_State1;
     Button m_MS_Button1;
@@ -27,27 +25,24 @@ public class MainScreen : MenuScreen
     protected override void SetVisualElements()
     {
         base.SetVisualElements();
-        m_MS_Popup1 = m_Root.Q<VisualElement>(MS_POPUP1);
-        m_MS_Popup2 = m_Root.Q<VisualElement>(MS_POPUP2);
+        m_MS_Popup = m_Root.Q<VisualElement>(MS_POPUP);
         m_MS_State0 = m_Root.Q<VisualElement>(MS_STATE0);
         m_MS_State1 = m_Root.Q<VisualElement>(MS_STATE1);
         m_MS_Button1 = m_Root.Q<Button>(MS_BUTTON1);
         m_MS_Button2 = m_Root.Q<Button>(MS_BUTTON2);
 
-        AnimateState();
+       
 
-    }
- 
+    } 
     private void AnimateState()
     {
-        m_MS_State0.ToggleInClassList("popup-state1");
-        m_MS_State1.RegisterCallback<TransitionEndEvent>(evt => m_MS_State0.ToggleInClassList("popup-state1"));
-        Debug.Log("asdf");
+        m_MS_State0.ToggleInClassList("popup--state1");
+        m_MS_State1.RegisterCallback<TransitionEndEvent>(evt => m_MS_State0.ToggleInClassList("popup--state1"));
     }
     protected override void RegisterButtonCallbacks()
     {
         base.RegisterButtonCallbacks();
-        AnimateState();
+        
 
         m_MS_Button1.RegisterCallback<ClickEvent>(evt => OnMainBt(0));
         m_MS_Button2.RegisterCallback<ClickEvent>(evt => OnMainBt(1));
@@ -64,6 +59,11 @@ public class MainScreen : MenuScreen
         {
             m_MainMenuUIManager.ShowSmartCityScreen();
         }
+    }
+
+    void Start()
+    {
+        AnimateState();
     }
 
 }
